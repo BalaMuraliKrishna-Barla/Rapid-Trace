a7bce6b28536a8996a04ddd73e2e6eaa3f8bf93a
Murali Krishna <krishnamurali.barla@sasi.ac.in>
2024-10-17 15:11:59 +0530

    Added frontend and backend folders with format

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import IndexPage from './pages/IndexPage'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import HomePage from './pages/HomePage'
+
+const App = () => {
+  return (
+    <div>
+      Start from here
+      <IndexPage />
+      <Signup />
+      <Login />
+
+      <HomePage />
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
